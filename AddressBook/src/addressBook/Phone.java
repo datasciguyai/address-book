a384c09a92c8250a00b49207078379b84f136a8c
rymerej <rymerej@hotmail.com>
2020-04-19 17:34:18 -0600

    Update Phone to string.isEmpty()

--- AddressBook/src/addressBook/Phone.java
+++ AddressBook/src/addressBook/Phone.java
@@ -86,7 +86,7 @@ public class Phone {
 	 * @return the String if not null, or blank text if so
 	 */
 	public String optional(String string) {
-		if (string == null)
+		if (string.isEmpty())
 			return "";
 		else
 			return toPhoneNumber(string);
